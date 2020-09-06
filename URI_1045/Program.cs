ed928935b8e8193e42615cbd9331d800c273f03b
Matheus Sousa <ma.sousa.soares@gmail.com>
2020-09-06 15:08:54 -0300

    Criado aplicação URI_1045

---
+++ URI_1045/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1045
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, c;
+
+            string [] vet = Console.ReadLine().Split(' '); 
+            a = float.Parse(vet[0],CultureInfo.InvariantCulture);
+            b = float.Parse(vet[1],CultureInfo.InvariantCulture);
+            c = float.Parse(vet[2],CultureInfo.InvariantCulture);
+            
+        }
+    }
+}
