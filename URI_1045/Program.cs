0b97042abbf5e1ecc0d553c154447f09bb1f123e
Matheus Sousa <ma.sousa.soares@gmail.com>
2020-09-06 16:07:50 -0300

    Revisado código

--- URI_1045/Program.cs
+++ URI_1045/Program.cs
@@ -14,6 +14,24 @@ namespace URI_1045
             b = float.Parse(vet[1],CultureInfo.InvariantCulture);
             c = float.Parse(vet[2],CultureInfo.InvariantCulture);
             
+            if (a >= b +c){
+                Console.WriteLine("NAO FORMA TRIANGULO");
+            }
+            else if (a * a == b * b + c * c){
+                Console.WriteLine("TRIANGULO RETANGULO");
+            }
+            else if ((a *a) > (b * b) + (c * c)){
+                Console.WriteLine("TRIANGULO OBTUSANGULO");
+            }
+            else if ((a * a) < (b * b) + (c * c)){
+                Console.WriteLine(" TRIANGULO ACUTANGULO");
+            }
+            else if (a == b && a == c){
+                Console.WriteLine("TRIANGULO EQUILATERO");
+            }
+            else if (a == b || b == c || a == c){
+                Console.WriteLine("TRIANGULO ISOSCELES");
+            }
         }
     }
 }
