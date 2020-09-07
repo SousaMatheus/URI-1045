e98a49445d61457b35f85b5af0bca591fd077e3d
Matheus Sousa <ma.sousa.soares@gmail.com>
2020-09-07 11:50:56 -0300

    Adicionado lógica para ordenar numeros de forma crescente e classificacao de triangulos

--- URI_1045/Program.cs
+++ URI_1045/Program.cs
@@ -7,29 +7,63 @@ namespace URI_1045
     {
         static void Main(string[] args)
         {
-            float a, b, c;
+            double a, b, c;
+            a =0; b =0; c=0;
 
             string [] vet = Console.ReadLine().Split(' '); 
-            a = float.Parse(vet[0],CultureInfo.InvariantCulture);
-            b = float.Parse(vet[1],CultureInfo.InvariantCulture);
-            c = float.Parse(vet[2],CultureInfo.InvariantCulture);
-            
-            if (a >= b +c){
+            double valor1 = float.Parse(vet[0],CultureInfo.InvariantCulture);
+            double valor2 = float.Parse(vet[1],CultureInfo.InvariantCulture);
+            double valor3 = float.Parse(vet[2],CultureInfo.InvariantCulture);
+
+            if (valor1 > valor2 && valor1 > valor3){
+                a = valor1;
+                if (valor2 > valor3){
+                    b = valor2;
+                    c = valor3;
+                } else {
+                    b = valor3;
+                    c = valor2;
+                }                
+            }
+            else if (valor2 > valor1 && valor2 > valor3){
+                a = valor2;
+                if ( valor1 > valor3){
+                b = valor1;
+                c = valor3;
+                } else {
+                    b = valor3;
+                    c = valor1;
+                }
+            }
+            else {
+                a = valor3;
+                if (valor1 > valor2){
+                b = valor1;
+                c = valor2;
+                } else {
+                    b = valor2;
+                    c = valor1;
+                }
+            }            
+            if (a >= b + c ){
                 Console.WriteLine("NAO FORMA TRIANGULO");
             }
-            else if (a * a == b * b + c * c){
+            else {
+                if (a * a == b * b + c * c){
                 Console.WriteLine("TRIANGULO RETANGULO");
             }
             else if ((a *a) > (b * b) + (c * c)){
                 Console.WriteLine("TRIANGULO OBTUSANGULO");
             }
-            else if ((a * a) < (b * b) + (c * c)){
-                Console.WriteLine(" TRIANGULO ACUTANGULO");
+            else //if ((a * a) < (b * b) + (c * c)){
+                {
+                Console.WriteLine("TRIANGULO ACUTANGULO");
             }
-            else if (a == b && a == c){
+            }
+             if (a == b && a == c){
                 Console.WriteLine("TRIANGULO EQUILATERO");
             }
-            else if (a == b || b == c || a == c){
+                else if (a == b || b == c || a == c){
                 Console.WriteLine("TRIANGULO ISOSCELES");
             }
         }
